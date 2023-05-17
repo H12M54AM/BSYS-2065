3e6501253f8769e410184216a09446b5a457717b
Edward Naidoo <securedbread@protonmail.com>
2023-05-17 10:19:28 -0700

    Completed Chapter 10

--- Chapter 10/ch10-12.py
+++ Chapter 10/ch10-12.py
@@ -1,7 +1,21 @@
 """
-Ch 10 Question 12 - 
+Ch 10 Question 12 - Count till 'Sam'
 ---
 Edward Naidoo
 BSYS-2065
 May 16, 2023
 """
+names = ['Jaime', 'Dan', 'Ken', 'Edward', 'Sam', 'Alen', 'Don']
+
+def count_names(nm:list) -> int:
+    """Takes a List and counts up unit it reaches 'sam'"""
+    count = 0
+    for i in nm:
+        if i.lower() != 'sam':
+            count += 1
+        else:
+            break
+    
+    return count
+
+print(count_names(names))
